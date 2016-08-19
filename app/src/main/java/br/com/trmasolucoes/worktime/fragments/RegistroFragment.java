7b03313989f6e76ec36ea322609d4531f3204d9f
tairoroberto <tairoroberto@hotmail.com>
2016-08-19 17:45:07 -0300

    Ajustando somatoria de horas

--- app/src/main/java/br/com/trmasolucoes/worktime/fragments/RegistroFragment.java
+++ app/src/main/java/br/com/trmasolucoes/worktime/fragments/RegistroFragment.java
@@ -11,18 +11,16 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.wdullaer.materialdatetimepicker.time.RadialPickerLayout;
-import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
-
+import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 
 import br.com.trmasolucoes.worktime.R;
 import br.com.trmasolucoes.worktime.adapters.RegistroAdapter;
+import br.com.trmasolucoes.worktime.database.HorarioDAO;
 import br.com.trmasolucoes.worktime.database.RegistroDAO;
 import br.com.trmasolucoes.worktime.domain.Registro;
 import br.com.trmasolucoes.worktime.interfaces.RecyclerViewOnClickListenerHack;
@@ -40,6 +38,9 @@ public class RegistroFragment extends Fragment implements RecyclerViewOnClickLis
     private RegistroAdapter adapter;
     private String data;
     private Registro registro;
+    private TextView txtSaldoHorasDia;
+    private TextView txtPrevisaoSaida;
+    private HorarioDAO horarioDAO;
 
     public static RegistroFragment newInstance(long date) {
         RegistroFragment fragment = new RegistroFragment();
@@ -74,6 +75,9 @@ public class RegistroFragment extends Fragment implements RecyclerViewOnClickLis
         final View view = inflater.inflate(R.layout.fragment_registro, container, false);
 
         mRecyclerView = (RecyclerView) view.findViewById(R.id.rv_list);
+        txtPrevisaoSaida = (TextView) view.findViewById(R.id.txtPrevisao);
+        txtSaldoHorasDia = (TextView) view.findViewById(R.id.txtSaldoDia);
+
         mRecyclerView.setHasFixedSize(true);
         mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
             @Override
@@ -94,11 +98,13 @@ public class RegistroFragment extends Fragment implements RecyclerViewOnClickLis
             public void onRefresh() {
                 // Refresh items
                 registroDAO = new RegistroDAO(getActivity());
+                horarioDAO = new HorarioDAO(getActivity());
                 final long millis = getArguments().getLong(KEY_DATE);
                 registros = registroDAO.getByDate(DateUtil.getFormattedDate(getActivity(), millis,null));
                 adapter = new RegistroAdapter(getActivity(), registros, getFragmentManager());
                 mRecyclerView.setAdapter(adapter);
                 mSwipeRefreshLayout.setRefreshing(false);
+                ajustarHorarioSaida(registros, registroDAO, horarioDAO);
             }
         });
         mRecyclerView.addOnItemTouchListener(new RecyclerViewTouchListener(getActivity(), mRecyclerView, this));
@@ -180,5 +186,58 @@ public class RegistroFragment extends Fragment implements RecyclerViewOnClickLis
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putParcelableArrayList("registros", (ArrayList<Registro>) registros);
+    }
+
+    private void ajustarHorarioSaida(List<Registro> registros, RegistroDAO registroDAO, HorarioDAO horarioDAO) {
+
+        float entrada = 0;
+        float almoco = 0;
+        float almocoRetorno = 0;
+        float saida = 0;
+        float total = 0;
+
+
+        for (Registro registro : registros) {
+            /** Separa a string "-" da data e coloca no textView */
+            String[] data = DateUtil.getDateToString(registro.getData()).split(" ");
+            String[] horario = data[1].split(":");
+
+            if (registro.getTipo().equalsIgnoreCase("entrada")){
+                entrada = getHorarioFloat(horario[0] +":"+ horario[1]);
+
+            }else if (registro.getTipo().equalsIgnoreCase("almoco")){
+                almoco = getHorarioFloat(horario[0] +":"+ horario[1]);
+
+            }else if (registro.getTipo().equalsIgnoreCase("almoco_retorno")){
+                almocoRetorno = getHorarioFloat(horario[0] +":"+ horario[1]);
+
+            }else if (registro.getTipo().equalsIgnoreCase("saida")){
+                saida = getHorarioFloat(horario[0] +":"+ horario[1]);
+            }
+
+        }
+
+        if (entrada > 0 && almoco > 0 && almocoRetorno > 0 && saida > 0){
+            total = (saida - almocoRetorno) + (almoco - entrada);
+
+        }else if (entrada > 0 && almoco > 0 && almocoRetorno > 0){
+            total = almocoRetorno + (almoco - entrada);
+
+        }else if (entrada > 0 && almoco > 0){
+            total = almoco - entrada;
+
+        }else {
+            total = 0;
+        }
+        DecimalFormat df = new DecimalFormat("0.00");
+        String.valueOf(df.format(total));
+
+    }
+
+    private static float getHorarioFloat(String horario){
+        if (horario == null){
+            return 0;
+        }
+        return Float.parseFloat(horario.replace(":","."));
     }
 }
